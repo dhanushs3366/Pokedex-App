b739f9df82f8f2a83bd1f1444371b245298af245
dhanushs3366 <dhanushs3366@gmail.com>
2024-06-27 12:55:27 +0530

    poke viewer header is styled

--- frontend/src/Components/PokemonViewer.tsx
+++ frontend/src/Components/PokemonViewer.tsx
@@ -25,11 +25,12 @@ const PokemonViewer: React.FC<PokemonViewerProps> = ({ imgSrc, canRender }) => {
   
 
   return (
-    <div className="img-header relative w-full h-full bg-white flex justify-center items-center  rounded-t-2xl">
-      <div className="img-type-bg absolute bottom-[30%]  w-[110%] h-[110%] bg-white rounded-full flex justify-center items-center ">
-        <div className="img-type-holder absolute size-[40%]  rounded-full z-20" style={applyOpacity("#ff9c54")}>
+    <div className="img-header relative w-full h-full bg-white flex justify-center items-center  rounded-t-2xl overflow-hidden">
+      <div className="img-type-bg absolute bottom-[45%]  w-[110%] h-[110%] bg-[#ff9c54] rounded-full flex justify-center items-center overflow-hidden">
+        <div className="img-type-holder absolute size-[40%] top-[55%] rounded-full z-20" style={applyOpacity("#ff9c54")}> 
         </div>
-        <img className="absolute poke-guess-img z-10 " src="frontend/src/assets/images/pokemon-types/FIRE.png"  alt="" />
+        {/* if the above div position changed change the image position too so it stacks on top of each other */}
+        <img className="absolute poke-guess-img z-10 top-[55%]" src="frontend/src/assets/images/pokemon-types/FIRE.png"  alt="" />
       </div>
     </div>
   );
